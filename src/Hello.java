bbb8486627f21a50ade961111dcf0d49db81d0d6
miguel diaz <diaz4674@gmail.com>
2021-07-26 20:59:13 -0700

    basics

---
+++ src/Hello.java
@@ -0,0 +1,41 @@
+public class Hello {
+    public static void main(String[] args) {
+        // bytes
+
+        int myValue = 10000;
+
+        int myMinInt = Integer.MIN_VALUE;
+        int myMaxInt = Integer.MAX_VALUE;
+
+        System.out.println("Integer Min = " + myMinInt );
+        System.out.println("Integer Max = " + myMaxInt );
+
+        int myMaxIntTest = 2147483647;
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short min= " + myMinShortValue);
+        System.out.println("Short max= " + myMaxShortValue);
+
+        long myLongValue =100L;
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Long min= " + myMinLongValue);
+        System.out.println("Long max= " + myMaxLongValue);
+
+
+
+        // intssss!
+
+//        int num = 3;
+//        int second = 3;
+//        int thrid = 3;
+//
+//        int total = num + second + thrid;
+//
+//        System.out.println("hello " + total);
+    }
+}

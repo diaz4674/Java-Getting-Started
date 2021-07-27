77ec9c4d791a17e0a6b88960f26e855e3c9fa068
miguel diaz <diaz4674@gmail.com>
2021-07-26 21:41:46 -0700

    strins

---
+++ src/CharAndBoolean.java
@@ -0,0 +1,22 @@
+public class CharAndBoolean {
+    public static void main (String[] args) {
+
+        char myChar = '\u0044';
+
+        System.out.println(myChar);
+
+        boolean myTrue = true;
+        boolean myFalse =false;
+
+        boolean isOverTwentyOne = true;
+
+//        strings!
+        String myString = "Thisis ak";
+
+        System.out.println(myString);
+
+        myString = myString + " hi";
+
+        System.out.println( myString);
+    }
+}
---
+++ src/FloatAndDouble.java
@@ -0,0 +1,26 @@
+
+public class FloatAndDouble {
+    public static void  main(String[] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+
+        System.out.println("Float Min Value = " + myMinFloatValue);
+        System.out.println("Float Max Value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+
+        System.out.println("Double Min Value = " + myMinDoubleValue);
+        System.out.println("Double Max Value = " + myMaxDoubleValue);
+
+        int myIntValue = 5 /3;
+        float myFloatValue = 5f/3f;
+        double myDoubleValue = 5d/3d;
+
+        System.out.println("MyIntVal " + myIntValue);
+        System.out.println("MyFloatVal " + myFloatValue);
+        System.out.println("myDoubleValue " + myDoubleValue);
+
+
+    }
+}
